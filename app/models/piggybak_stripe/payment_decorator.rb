9190cf4ebcd431069aa20be51102a68cf0624d90
Tim Case <tim@powerupdev.com>
2012-10-12 22:02:46 +0700

    Change decorator to extend ActiveSupport::Concern

---
+++ app/models/piggybak_stripe/payment_decorator.rb
@@ -0,0 +1,40 @@
+module PiggybakStripe
+  module PaymentDecorator
+    extend ActiveSupport::Concern
+    
+    attr_accessor :stripe_token
+    attr_accessible :stripe_token
+
+    validates_presence_of :stripe_token
+
+    [:month, :year, :payment_method_id].each do |field|
+      _validators.reject!{ |key, _| key == field }
+
+      _validate_callbacks.reject! do |callback|
+        callback.raw_filter.attributes == [field]
+      end  
+    end
+
+    def process
+      calculator = ::Piggybak::PaymentCalculator::Stripe.new(self.payment_method)
+      Stripe.api_key = calculator.secret_key
+      begin
+        charge = Stripe::Charge.create({
+                    :amount => self.order.total_due * 100,
+                    :card => self.stripe_token,
+                    :currency => "usd"
+                  })
+
+        self.attributes = { :total => self.order.total_due, 
+                            :transaction_id => charge.id,
+                            :masked_number => charge.card.last4 }
+        return true
+      rescue Stripe::CardError, Stripe::InvalidRequestError => e
+        self.errors.add :payment_method_id, e.message
+        return false
+      end
+  
+    end
+  end
+end
+  
